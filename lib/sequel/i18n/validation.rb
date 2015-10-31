d2316760de1f619275d8ba42412e6bd1a9dc0169
sven199109 <sven199109@gmail.com>
2015-10-31 08:46:13 -0700

    完成

--- lib/sequel/i18n/validation.rb
+++ lib/sequel/i18n/validation.rb
@@ -3,7 +3,7 @@ class Sequel::I18n::Validation
   class << self 
     def load 
       validation_options = ::Sequel::Plugins::ValidationHelpers::DEFAULT_OPTIONS.dup
-      [:format, :integer, :length_range,:not_null, :numeric, :presence, :unique].each do |type|
+      [:integer, :length_range,:not_null, :numeric, :presence, :unique].each do |type|
         validate_no_params(type) do 
           begin
             I18n.t!("errors.#{type.to_s}")
@@ -12,7 +12,7 @@ class Sequel::I18n::Validation
           end
         end
       end
-      [:exact_length, :max_length, :min_length, :type, :includes].each do |type|
+      [:format, :exact_length, :max_length, :min_length, :type, :includes].each do |type|
         validate_has_block(type) do |arg|
           begin
             ::I18n.t!("errors.#{type.to_s}", arg: arg) 
@@ -32,7 +32,7 @@ class Sequel::I18n::Validation
 
     def validate_no_params(field, &block)
       ::Sequel::Plugins::ValidationHelpers::DEFAULT_OPTIONS.merge!(
-        field => {message: block}
+        field => {message: block.call}
       )
     end
 
