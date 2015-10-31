5683ed8a7d28d2d8d0a0062c6f68b3b04ad5310b
sven199109 <sven199109@gmail.com>
2015-10-31 06:39:08 -0700

    test for validation

---
+++ lib/sequel/i18n/validation.rb
@@ -0,0 +1,36 @@
+module Sequel 
+  module I18n 
+    module Validation 
+      class << self 
+        def load 
+          [:format, :integer, :length_range,:not_null, :numeric, :presence, :unique].each do |type|
+            begin
+              message = ::I18n.t!("sequel.errors.#{type.to_s}")
+              validate_no_params(type, message)
+            end
+          end
+          [:exact_length, :max_length, :min_length, :type].each do |type|
+            validate_has_block do |arg|
+              begin
+                ::I18n.t!("sequel.errors.#{type.to_s}", arg: arg)
+              rescue
+              end
+            end
+          end
+        end
+
+        def validate_no_params(field, message)
+          ::Sequel::Plugins::ValidationHelpers::DEFAULT_OPTIONS.merge!(
+            field => {message: message}
+          )
+        end
+
+        def validate_has_block(&block)
+          ::Sequel::Plugins::ValidationHelpers::DEFAULT_OPTIONS.merge!(
+            field => {message: block}
+          )
+        end
+      end
+    end
+  end
+end
