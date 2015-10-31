7987d844ad8ea92c0e603a99000d289b2bb24f6f
sven199109 <sven199109@gmail.com>
2015-10-31 05:55:29 -0700

    实现model字段i18n

--- lib/sequel/i18n/errors.rb
+++ lib/sequel/i18n/errors.rb
@@ -3,7 +3,8 @@ class Sequel::Model::Errors
   def full_messages
     inject([]) do |m, kv|
       att, errors = *kv
-      att.is_a?(Array) ? Array(att).map! { |v| I18n.t("#{model_name}.attributes.#{v}")} : att = I18n.t("#{model_name}.attributes.#{att}")
+      attr_name = model_name.to_s.downcase
+      att.is_a?(Array) ? Array(att).map! { |v| I18n.t("#{attr_name}.attributes.#{v}")} : att = I18n.t("#{attr_name}.attributes.#{att}")
       errors.each {|e| m << (e.is_a?(::Sequel::LiteralString) ? e : "#{Array(att).join(ATTRIBUTE_JOINER)} #{e}")}
       m
     end
