eb3bae54f182cb10bf9d14ed16296db490c6737f
sven199109 <sven199109@gmail.com>
2015-10-31 06:49:43 -0700

    test for validation

--- lib/sequel/i18n.rb
+++ lib/sequel/i18n.rb
@@ -1,9 +1,12 @@
 require "sequel/i18n/version"
 require "sequel/i18n/errors"
 require "sequel/i18n/model"
+require "sequel/i18n/validation"
 
 module Sequel
   module I18n
     # Your code goes here...
   end
 end
+
+Sequel::I18n::Validation.load
