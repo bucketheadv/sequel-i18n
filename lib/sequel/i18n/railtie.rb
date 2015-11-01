8fea2129362af66b2d2cdbd62bebe13eb2c25ba9
sven199109 <sven199109@gmail.com>
2015-10-31 20:53:10 -0700

    rails加载config/locales下所有yml

--- lib/sequel/i18n/railtie.rb
+++ lib/sequel/i18n/railtie.rb
@@ -3,7 +3,7 @@ module Sequel
     class Railtie < ::Rails::Railtie 
       initializer 'sequel-i18n' do |_app|
         ::Sequel::I18n::Validation.load
-        ::I18n.load_path += Dir[File.expand_path("../config/locales/**/*.yml", Rails.root)]
+        ::I18n.load_path += Dir[File.expand_path("./config/locales/**/*.yml", Rails.root)]
       end
     end
   end
